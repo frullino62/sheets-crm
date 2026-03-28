7e020da59e716ee44572a0c2182e535bb46ca257
frullino62 <43367410+frullino62@users.noreply.github.com>
2026-03-28 11:36:53 +0100

    Update sheets_db.py

--- sheets_db.py
+++ sheets_db.py
@@ -5,25 +5,26 @@ from google.oauth2.service_account import Credentials
 
 SHEET_ID = "10EU1gQSBbgA3k-n2ltzlJqW54NQ1aqBobHpvPdOfmsU"
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
-def get_client():
+def get_sheet():
     creds_json = os.getenv("GOOGLE_CREDENTIALS")
+
+    if not creds_json:
+        raise Exception("GOOGLE_CREDENTIALS non trovato")
+
     creds_dict = json.loads(creds_json)
 
     creds = Credentials.from_service_account_info(
         creds_dict,
-        scopes=SCOPES   # 🔥 QUESTA È LA FIX
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
     )
 
-    return gspread.authorize(creds)
-
-def sheet():
-    client = get_client()
+    client = gspread.authorize(creds)
     return client.open_by_key(SHEET_ID).sheet1
 
+
 def get_all():
-    return sheet().get_all_records()
+    sheet = get_sheet()
+    return sheet.get_all_records()
