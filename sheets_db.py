6012ce69f95a42bd3097424adb2b5a1cf4acd98f
frullino62 <43367410+frullino62@users.noreply.github.com>
2026-03-28 10:53:06 +0100

    Update sheets_db.py

--- sheets_db.py
+++ sheets_db.py
@@ -1,36 +1,19 @@
+import os
+import json
 import gspread
 from google.oauth2.service_account import Credentials
-import os
 
-SHEET_ID = os.environ["SHEET_ID"]
+SHEET_ID = "10EU1gQSBbgA3k-n2ltzlJqW54NQ1aqBobHpvPdOfmsU"
 
-scope = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
-creds = Credentials.from_service_account_file(
-    "credentials.json",
-    scopes=scope
-)
-
-client = gspread.authorize(creds)
+def get_client():
+    creds_json = os.getenv("GOOGLE_CREDENTIALS")
+    creds_dict = json.loads(creds_json)
+    creds = Credentials.from_service_account_info(creds_dict)
+    return gspread.authorize(creds)
 
 def sheet():
+    client = get_client()
     return client.open_by_key(SHEET_ID).sheet1
-
 
 def get_all():
     return sheet().get_all_records()
-
-
-def add(nome, email, telefono):
-    sheet().append_row([nome, email, telefono])
-
-
-def update(row, nome, email, telefono):
-    sheet().update(f"A{row}:C{row}", [[nome, email, telefono]])
-
-
-def delete(row):
-    sheet().delete_rows(row)
