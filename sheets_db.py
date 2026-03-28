4d1f3b1c26822923c35da67212aae1c5c79a5ceb
frullino62 <43367410+frullino62@users.noreply.github.com>
2026-03-28 12:51:52 +0100

    Update sheets_db.py

--- sheets_db.py
+++ sheets_db.py
@@ -1,3 +1,5 @@
+import os
+import json
 from google.oauth2.service_account import Credentials
 import gspread
 
@@ -8,25 +10,11 @@ SCOPES = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    "credentials.json",
+creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+
+creds = Credentials.from_service_account_info(
+    creds_dict,
     scopes=SCOPES
 )
 
 client = gspread.authorize(creds)
-
-
-def sheet():
-    return client.open_by_key(SHEET_ID).sheet1
-
-
-def get_all():
-    return sheet().get_all_records()
-
-
-def add_cliente(nome, email):
-    sheet().append_row([nome, email])
-
-
-def delete_cliente(index):
-    sheet().delete_rows(index + 1)  # +1 perché header
