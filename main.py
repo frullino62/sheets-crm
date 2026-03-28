239c2a52e072abe88ee79230ce6ad99f17e450fa
frullino62 <43367410+frullino62@users.noreply.github.com>
2026-03-28 14:36:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,28 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def home(request: Request):
     clienti = sheets_db.get_all()
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "clienti": clienti
-    })
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "clienti": clienti
+        }
+    )
+
+
+@app.get("/test")
+def test():
+    try:
+        data = sheets_db.get_all()
+        return {
+            "status": "success",
+            "records": data
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
 
 
 @app.get("/add")
