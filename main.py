f652bebf559bcb1fd7b7b23102691b36302492be
frullino62 <43367410+frullino62@users.noreply.github.com>
2026-03-29 09:03:31 +0200

    Refactor main.py for code organization and clarity

--- main.py
+++ main.py
@@ -3,18 +3,14 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from auth import router as auth_router
 import sheets_db
+import os
 
 app = FastAPI()
 
 app.include_router(auth_router)
 
-from jinja2 import Environment, FileSystemLoader
-
-import os
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
-templates.env = Environment(loader=FileSystemLoader("templates"), auto_reload=True)
 
 
 @app.get("/")
@@ -22,7 +18,7 @@ def home(request: Request):
     try:
         clienti = sheets_db.get_all()
     except Exception as e:
-        return {"errore": str(e)}  # debug
+        return {"errore": str(e)}
 
     return templates.TemplateResponse(
         "index.html",
@@ -32,12 +28,14 @@ def home(request: Request):
         }
     )
 
+
 @app.get("/login")
 def login_page(request: Request):
     return templates.TemplateResponse(
         "login.html",
         {"request": request}
     )
+
 
 @app.get("/add")
 def add(nome: str, email: str):
