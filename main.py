8e4deff53369ceacb6275f5657d45d3e78ffe6ff
frullino62 <43367410+frullino62@users.noreply.github.com>
2026-03-29 09:36:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,23 +17,8 @@ import os
 print("FILES:", os.listdir("templates"))
 
 @app.get("/")
-def home(request: Request):
-    try:
-        clienti_raw = sheets_db.get_all()
-
-        # 🔴 FIX CRITICO
-        clienti = [dict(c) for c in clienti_raw]
-
-    except Exception as e:
-        return {"errore": str(e)}
-
-    return templates.TemplateResponse(
-        "index.html",
-        {
-            "request": request,
-            "clienti": clienti
-        }
-    )
+def home():
+    return {"ok": True}
 
 
 @app.get("/login")
